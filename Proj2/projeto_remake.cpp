d6279155bf5fa252928943b1caa57393ae49ead3
Miguel_Sol <migueldsol@tecnico.ulisboa.pt>
2023-01-02 16:28:42 +0000

    not working

--- Proj2/projeto_remake.cpp
+++ Proj2/projeto_remake.cpp
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <unordered_map>
 #include <vector>
+#include <stdio.h>
 
 using namespace std;
 
@@ -12,35 +13,46 @@ using namespace std;
 // and the weight of the edge
 vector<tuple<int, int, int>> edges;
 
-vector<tuple<int,int>> parent_rank;
+vector<int> parent;
+
+vector<int> vertex_rank;
+
+// Initialize the minimum spanning tree
+//vector<tuple<int, int, int>> mst;
 
 int final_edges = 0;
 int final_weight = 0;
 
 // Find the set that a particular vertex belongs to
-int find_set(int vertex, int size) {
-  for (int i = 1; i <= size; i++) {
-    if (get<1>(parent_rank[vertex]) != vertex) {
-      ;
-    }
+//TODO:Path compresion
+int find_set(int vertex) {
+  int num_parent = parent[vertex];
+  if ( num_parent != vertex){
+    return find_set(num_parent);
   }
-  return -1;
+  return num_parent;
+}
+
+int get_rank(int vertex){
+  return vertex_rank[vertex];
 }
 
 int main() {
   // Read the number of nodes and edges
   int num_nodes;
-  cin >> num_nodes;
-
   int num_edges;
-  cin >> num_edges;
+  scanf("%d",&num_nodes);
+  scanf("%d",&num_edges);
 
   // Read the edges and add them to the adjacency list
   for (int i = 0; i < num_edges; i++) {
+    printf("entrei");
     int u, v, w;
-    cin >> u >> v >> w;
-    edges.emplace_back(u, v, w);
+    scanf("%d %d %d", &u, &v, &w);
+    edges.push_back(tuple<int, int, int>{u, v, w});
+    printf("sai");
   }
+  printf("sai");
 
   // Sort the edges in ascending order by weight
     sort(edges.begin(), edges.end(), [](tuple<int, int, int>& a, tuple<int, int, int>& b) {
@@ -55,42 +67,40 @@ int main() {
     } */
 
   // Initialize the vector of sets with a set for each vertex
-  for (int i = 1; i <= num_nodes; i++) {
-    parent_rank.emplace_back(i,0);
+  for (int i = 1; i <= num_nodes; i++){
+    parent.push_back({i});
+    vertex_rank.push_back({0});
+    printf("parent[%d] = %d", i, parent[i]);
   }
-
-  // Initialize the minimum spanning tree
-  vector<tuple<int, int, int>> mst;
+  printf("sai");
   // Iterate through the sorted edges
   for (auto& e : edges) {
     int u = get<0>(e);
     int v = get<1>(e);
     int w = get<2>(e);
     // Find the sets that contain the vertices that the edge connects
-    int set_u = find_set(u,num_nodes);
-    int set_v = find_set(v,num_nodes);
-    if(set_u == -1 || set_v == -1){
-        break;
+    int set_u = find_set(u);
+    int set_v = find_set(v);
+    int rank_u = get_rank(u);
+    int rank_v = get_rank(v);
+    if(set_u != set_v){
+      final_edges++;
+      final_weight += w;
+      if(rank_u > rank_v){
+        parent[set_v] = set_u;
+      }else{
+        parent[set_u] = set_v;
+        if(rank_u == rank_v){
+          vertex_rank[set_v]++;
+        }
+      }
     }
-    // If the sets are different, merge the sets and add the edge to the minimum spanning tree
-    if (set_u != set_v) {
-        mst.emplace_back(u, v, w);
-        sets[set_u].insert(sets[set_v].begin(), sets[set_v].end());
-        sets.erase(sets.begin() + set_v);
-    }
-
     // Stop when all the vertices are in the same set, or when we have added all the edges to the minimum spanning tree
-    if (sets.size() == 1 || mst.size() == num_nodes - 1) {
-    break;
+    if (final_edges == num_nodes - 1) {
+      break;
     }
 }
-    cout << mst.size() << endl;
-    for (auto& e : mst) {
-        int u = get<0>(e);
-        int v = get<1>(e);
-        int w = get<2>(e);
-        cout << "(" << u << ", " << v << ", " << w << ")" << endl;
-    }
+    cout << final_weight << endl;
 
   return 0;
 }
